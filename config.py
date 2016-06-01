6b67cdd7485c245f20db0484c6b7dc4c81159c2a
Patrik Helia <phelia@redhat.com>
2016-06-01 13:23:31 +0200

    Aggregation for Traces occuring on RHEL that are fixed in Fedora
    Merge with master and jfilak's patch and implement output for
    'Traces occuring on RHEL that are fixed in Fedora'
    
    Signed-off-by: Patrik Helia <phelia@redhat.com>

--- config.py
+++ config.py
@@ -1,18 +1,19 @@
 CACHE = True
 
-MASTER_URL = "http://10.34.24.173/faf/"
+#TODO Disable cache
 
-VERBOSE = True
+MASTER = "http://example.com/faf/"
 
 BUG_TYPE = ['ERRATA']  # Bugzilla bugs with closed Fedora Bugzilla bugs
 
 OPSYS = ['CentOS', 'Fedora', 'Red Hat Linux Enterprise']
 
-if False:
-    SLAVE = {'request_server_min': "http://10.34.24.108:8080/faf/",
-             'request_server_min_2': "http://10.34.24.108:8080/faf/"
-             }
+BZ_USER = ""
+BZ_PASSWORD = ""
 
-else:
-    SLAVE = {'request_server': "http://10.34.24.173//faf/"}
-    # SLAVE = {'request_server_min': "http://10.34.24.173/faf/"}
+SLAVE = {'request_server': "http://example.com/faf/"}
+
+# Example for multiple servers
+# SLAVE = {'request_server_min': "http://example.com/faf/",
+#           'request_server_min_2': "http://example1.com/faf/"}
+
