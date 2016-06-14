93cfc8eb266e33b8a442666547ffe2faa6c8f858
Patrik Helia <phelia@redhat.com>
2016-06-14 12:52:58 +0200

    Rewrete output to new template
    With jfilak we specified new format which include all specification froms
    comments.
    
    Signed-off-by: Patrik Helia <phelia@redhat.com>

--- config.py
+++ config.py
@@ -12,4 +12,3 @@ SLAVE = {'request_server': "http://example.com/faf/"}
 # Example for multiple servers
 # SLAVE = {'request_server_min': "http://example.com/faf/",
 #           'request_server_min_2': "http://example1.com/faf/"}
-
